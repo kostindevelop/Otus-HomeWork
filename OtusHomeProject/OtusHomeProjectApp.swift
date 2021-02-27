6944059e992e44a16df65652fc4cd8b11a9129dc
Kostindevelop <kostindevelop@gmail.com>
2021-02-28 01:24:12 +0200

    add project files

---
+++ OtusHomeProject/OtusHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtusHomeProjectApp.swift
+//  OtusHomeProject
+//
+//  Created by Kostindevelop on 28.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct OtusHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
